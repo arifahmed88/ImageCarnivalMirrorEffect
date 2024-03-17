ab47b9c3eea12b8a9902afbe0fcb0de9cf6ff3a7
arifahmed88 <arifahmedauny@gmail.com>
2024-03-17 13:54:46 +0600

    Initial Commit

---
+++ CarnivalMirrorEffect/CarnivalMirrorEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnivalMirrorEffectApp.swift
+//  CarnivalMirrorEffect
+//
+//  Created by ArifAhmed on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CarnivalMirrorEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
